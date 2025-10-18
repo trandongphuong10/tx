f0a2b405d2978272b3e86ebe286bab75d0a1b010
trandongphuong10 <trandongphuong.bot3@gmail.com>
2025-10-19 00:10:14 +0700

    first commit

---
+++ app/Models/TaiXiu_one.js
@@ -0,0 +1,24 @@
+let mongoose = require('mongoose');
+
+let Schema = new mongoose.Schema({
+	uid:      {type: String,  required: true, index: true}, // ID Người cược 
+	phien:    {type: Number,  required: true, index: true}, // phiên cược
+	bet:      {type: Number,  required: true},    // số tiền cược
+	select:   {type: Boolean, required: true},    // bên cược  (Tài = true, Xỉu = false)
+	tralai:   {type: Number,  default: 0},        // Số tiền trả lại
+	thuong:   {type: Number,  default: 0},        // Thưởng Red khi chơi bằng xu
+	win:      {type: Boolean, default: false},	  // Thắng hoặc thua
+	betwin:   {type: Number,  default: 0},	      // Tiền thắng được
+});
+
+// 👉 Tự động x2 tiền khi thắng
+Schema.pre('save', function(next) {
+	if (this.win === true) {
+		this.betwin = this.bet * 2;
+	}
+	next();
+});
+
+Schema.index({uid:1, phien:1}, {background: true});
+
+module.exports = mongoose.model('TaiXiu_one', Schema);
---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
