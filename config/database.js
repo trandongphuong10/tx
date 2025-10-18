91b2ce88225488b538234d471afeec6cfbdada23
trandongphuong10 <trandongphuong.bot3@gmail.com>
2025-10-19 01:06:47 +0700

    update MongoDB config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'mongodb+srv://pptx:19042010Pp_@tx.xqbydp9.mongodb.net/?retryWrites=true&w=majority&appName=TX',
+  url: 'mongodb+srv://pptx:19042010@tx.xqbydp9.mongodb.net/?retryWrites=true&w=majority&appName=TX',
   options: {
     dbName: 'CLUB3333', // hoặc tên database bạn muốn dùng
     useNewUrlParser: true,
